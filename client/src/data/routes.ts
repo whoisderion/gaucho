e48cf3fc19f2a5d5b80dc5456ca726696ccce59a
Derion Williams <derionwllms@gmail.com>
2023-08-07 19:05:37 -0500

    created routes file for managing URLs

--- client/src/data/routes.ts
+++ client/src/data/routes.ts
@@ -3,3 +3,6 @@ export const SIGN_UP = "/signup";
 export const SIGN_IN = "/signin";
 export const ACCOUNT = "/account";
 export const LANDING = "/";
+export const CREATE_FLEET = "/create-fleet"
+export const CREATE_INVENTORY = "/create-inventory"
+export const PRINT_QR_CODES = "/print-qr-codes"
