37fce6a3b884777f388749216dc0371910a397e2
Derion Williams <derionwllms@gmail.com>
2023-08-17 23:56:46 -0500

    Made a plan for the QR code page

--- client/src/pages/SignUp/QRCodes.tsx
+++ client/src/pages/SignUp/QRCodes.tsx
@@ -1,5 +1,14 @@
 function QRCodes() {
+
+    // make a request for the qr codes for every truck
+
+    // make a print all QRs function
+
+    // make a print selected QR function
+
     return (
+        // display the truck name, QR code, and a print button for every truck created
+
         <div>QRCodes</div>
     )
 }
