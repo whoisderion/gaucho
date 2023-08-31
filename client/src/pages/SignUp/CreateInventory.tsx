9d71f0146321be9b02b9d18ce653298ec8ed23ca
Derion Williams <derionwllms@gmail.com>
2023-08-31 10:36:34 -0500

    Initialized page to retreive fleet data

--- client/src/pages/SignUp/CreateInventory.tsx
+++ client/src/pages/SignUp/CreateInventory.tsx
@@ -1,6 +1,31 @@
+import { useNavigate } from "react-router-dom"
+import * as ROUTES from 'data/routes'
+import { useEffect, useState } from "react"
+
 function CreateInventory() {
+    useEffect(() => {
+        const fleetsData = sessionStorage.getItem("fleetsData")
+        if (fleetsData) {
+            let parsedData = JSON.parse(fleetsData)["fleets"]
+            setData(parsedData)
+        }
+    }, [])
+
+    const [data, setData] = useState("")
+    const navigate = useNavigate()
+
+    function continueSignup() {
+        navigate(ROUTES.PRINT_QR_CODES)
+    }
+
+    console.log(data)
+
+
     return (
-        <div>CreateInventory</div>
+        <>
+            <div>Create Inventory</div>
+            <button onClick={continueSignup}>Complete</button>
+        </>
     )
 }
 
