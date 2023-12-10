46469ff8dcbab8dfacd17bc4eb87b0e4f3e71fa3
Derion Williams <derionwllms@gmail.com>
2023-12-09 21:31:36 -0600

    Added functionality for submitting form data

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,35 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import * as ROUTES from 'data/routes'
+import axios from "axios";
 
 function SignUp() {
 
     const navigate = useNavigate();
 
-    function continueSignUp() {
-        // create account in firebase
-        // save new account to database
-        // continue to next page
-        console.log('submitted')
+    const formSubmit = async (e) => {
+        e.preventDefault()
+        let data = new FormData(e.target)
+        let formObject = Object.fromEntries(data.entries())
+        console.log(formObject['company-name'], formObject['company-email'], formObject['phone-number'])
+        await axios.post(`${import.meta.env.VITE_SERVER_URL}create-account`, {
+            companyName: formObject['company-name'],
+            email: formObject['company-email'],
+            phoneNumber: formObject['phone-number']
+        })
+
         return navigate(ROUTES.CREATE_FLEET)
     }
 
     return (
         <div>
-            <form>
+            <form onSubmit={formSubmit}>
                 <div>
                     <p>Company Name</p>
-                    <input type="text" name="company-name" id="name" />
+                    <input type="text" name="company-name" id="company-name" />
                 </div>
                 <div>
                     <p>Company Phone Number</p>
-                    <input type="text" name="phone-number" id="phone" />
+                    <input type="tel" name="phone-number" id="phone-number" />
                 </div>
                 <div>
                     <p>E-mail</p>
-                    <input type="text" name="company-email" id="email" ></input>
+                    <input type="text" name="company-email" id="company-email" ></input>
                 </div>
                 <div>
-                    <input type="button" value="Create your fleet" onClick={continueSignUp} />
+                    <input type="submit" value="Create your fleet" />
                 </div>
             </form>
         </div>
