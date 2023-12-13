fd9cf7637b708f939b50b05e0bb090cad971e27b
Derion Williams <derionwllms@gmail.com>
2023-12-13 11:32:50 -0600

    Added sidebar

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Sidebar from 'components/Sidebar'
 import { useEffect, useState } from "react"
 
 function Account() {
@@ -7,7 +8,7 @@ function Account() {
 
     useEffect(() => {
         async function getCompanyInfo() {
-            await axios.get(import.meta.env.VITE_SERVER_URL + 'account/company?companyID=clk7h1bi10000ztnbzju9lf02')
+            await axios.get(import.meta.env.VITE_SERVER_URL + 'account/company?companyID=clpw9jgxd0000ztvheigeqjgf')
                 .then(function (res: any) {
                     setCompanyInfo(res.data)
                 })
@@ -17,15 +18,24 @@ function Account() {
 
     if (companyInfo) {
         return (
-            <div>
-                <h1>Account</h1>
-                <div>
-                    <p>Created: </p>
-                    <p>Primary Email: {companyInfo.email}</p>
-                    <p>Company ID: {companyInfo.id}</p>
-                    <p>Company Name: {companyInfo.name}</p>
-                    <p>Phone Number: {companyInfo.phoneNumber}</p>
+            <div className='Contents'>
+                <Sidebar />
+                <div className="Settings inline-flex flex-col">
+                    <h2 >Account</h2>
+                    <div>
+                        <p>Created: </p>
+                        <p>Primary Email: {companyInfo.email}</p>
+                        <p>Company ID: {companyInfo.id}</p>
+                        <p>Company Name: {companyInfo.name}</p>
+                        <p>Phone Number: {companyInfo.phoneNumber}</p>
+                    </div>
                 </div>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Sidebar />
             </div>
         )
     }
