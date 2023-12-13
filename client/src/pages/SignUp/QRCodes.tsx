11d76486ebfc6b98350efbb91c649789e6727d9a
Derion Williams <derionwllms@gmail.com>
2023-12-13 11:34:08 -0600

    Implemented logic for saving the fleet data to the database and retreiving the QR code

--- client/src/pages/SignUp/QRCodes.tsx
+++ client/src/pages/SignUp/QRCodes.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom"
 import * as ROUTES from "data/routes"
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
+import QRCode from "qrcode"
 
 function QRCodes() {
 
@@ -12,46 +13,93 @@ function QRCodes() {
     // make a print selected QR function
 
     const currCompanyID = "clpw9jgxd0000ztvheigeqjgf"
+    const [QRData, setQRData] = useState<string[]>([])
 
+    const canvasRef = useRef(null)
+    const fleetsData = sessionStorage.getItem("fleetsData")
+    const parsedData = fleetsData ? JSON.parse(fleetsData) : "Not Found"
+
+    // fetch data
     useEffect(() => {
-        const fleetsData = sessionStorage.getItem("fleetsData")
-        if (fleetsData) {
-            const parsedData = JSON.parse(fleetsData)
-            console.log(parsedData)
 
-            const fleet = axios.post(`${import.meta.env.VITE_SERVER_URL}setup/fleets`, {
-                fleets: parsedData.fleets,
-                companyID: currCompanyID
-            })
-                .then(data => {
-                    console.log(data)
-                    const inventory = axios.post(`${import.meta.env.VITE_SERVER_URL}setup/inventory`, {
+        const fetchData = async () => {
+            try {
+                if (fleetsData) {
+                    // console.log(parsedData)
+
+                    const fleet = axios.post(`${import.meta.env.VITE_SERVER_URL}setup/fleets`, {
                         fleets: parsedData.fleets,
-                        equipmentTypes: parsedData.equipmentTypes,
                         companyID: currCompanyID
                     })
-                        .then(idata => {
-                            const retreiveQRCodes = axios.get(`${import.meta.env.VITE_SERVER_URL}print-qr-codes/${currCompanyID}`)
-                                .then(res => console.log(res))
+                        .then(fleetData => {
+                            const inventory = axios.post(`${import.meta.env.VITE_SERVER_URL}setup/inventory`, {
+                                fleets: parsedData.fleets,
+                                equipmentTypes: parsedData.equipmentTypes,
+                                companyID: currCompanyID
+                            })
+                                .then(inventoryData => {
+                                    const retreiveQRCodes = axios.get(`${import.meta.env.VITE_SERVER_URL}print-qr-codes/${currCompanyID}`)
+                                        .then(res => {
+                                            setQRData(res.data)
+                                        })
+                                        .catch(e => console.error(e))
+                                })
                                 .catch(e => console.error(e))
                         })
                         .catch(e => console.error(e))
-                })
-                .catch(e => console.error(e))
-
-
-            return () => {
-                fleet
+                }
+            } catch (error) {
+                console.log(error)
             }
         }
+
+        fetchData()
     }, [])
 
+    // create elements for qr codes
+    useEffect(() => {
+        const vehicleNames: string[] = []
+        if (fleetsData) {
+            for (const fleet of parsedData.fleets) {
+                for (const vehicle of fleet.vehicles) {
+                    vehicleNames.push(vehicle.name)
+                }
+            }
+        }
+
+        if (QRData.length > 0 && canvasRef.current && canvasRef.current != null) {
+            QRData.forEach((data, index) => {
+                const containerDiv = document.createElement("div")
+                // containerDiv.className = " flex"
+                const canvas = document.createElement("canvas")
+
+                QRCode.toCanvas(canvas, data, function (error) {
+                    if (error) console.error(error)
+                    // console.log(`QR Code ${index + 1} generated successfully!`)
+                })
+
+                const QRLabel = document.createElement("p")
+                QRLabel.textContent = vehicleNames[index]
+                QRLabel.className = ""
+
+                const printButton = document.createElement("button")
+                printButton.textContent = "Print"
+                printButton.className = ""
+
+                containerDiv.appendChild(QRLabel)
+                containerDiv.appendChild(canvas)
+                containerDiv.appendChild(printButton)
+                canvasRef.current.appendChild(containerDiv)
+            })
+        }
+
+    }, [QRData])
 
     return (
         // display the truck name, QR code, and a print button for every truck created
-
         <div>
             <div>QR Codes</div>
+            <div ref={canvasRef} id="qr-codes-container" className="flex" />
             <Link to={ROUTES.FLEET_OVERVIEW}><button>View your compnay dashboard</button></Link>
         </div>
     )
