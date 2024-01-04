1cc97265b03351268399e10a5b57fc6a8686129a
Derion Williams <derionwllms@gmail.com>
2024-01-03 23:42:43 -0600

    Updated routing to reflect changes in available pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,27 @@ import * as ROUTES from 'data/routes.ts'
 
 import Landing from 'pages/Landing'
 import SignUp from 'pages/SignUp/SignUp'
-import Account from 'pages/Account'
+import Account from 'pages/Settings/Account'
 import CreateFleet from 'pages/SignUp/CreateFleet'
 import CreateInventory from 'pages/SignUp/CreateInventory'
-import QRCodes from 'pages/SignUp/QRCodes'
+import SignUpQRCodes from 'pages/SignUp/QRCodes'
 import FleetOverview from 'pages/Dashboards/FleetOverview'
 import Maintenance from 'pages/Dashboards/Maintenance'
 import UserActivity from 'pages/Dashboards/UserActivity'
 import EquipmentDashboard from 'pages/Dashboards/EquipmentDashboard'
-import FleetManagement from 'pages/FleetManagement'
+import FleetManagement from 'pages/Settings/FleetManagement'
+import QRCodes from 'pages/QRCodes'
+import Upload from 'pages/Upload'
+import MaintenanceSettings from 'pages/Settings/MaintenanceSettings'
 
 function App() {
 
   return (
     <Router>
       <div className="App w-screen h-screen overflow-hidden">
+        <nav className=' border-solid border-ternary border-b-2 '>
+          <h2>Gaucho</h2>
+        </nav>
         <Routes>
           <Route
             path={ROUTES.LANDING}
@@ -37,13 +43,16 @@ function App() {
             element={<CreateInventory />} />
           <Route
             path={ROUTES.PRINT_QR_CODES}
-            element={<QRCodes />} />
+            element={<SignUpQRCodes />} />
           <Route
             path={ROUTES.ACCOUNT}
             element={<Account />} />
           <Route
             path={ROUTES.FLEET_MANAGEMENT}
             element={<FleetManagement />} />
+          <Route
+            path={ROUTES.MAINTENANCE_SETTINGS}
+            element={<MaintenanceSettings />} />
           <Route
             path={ROUTES.FLEET_OVERVIEW}
             element={<FleetOverview />} />
@@ -56,6 +65,12 @@ function App() {
           <Route
             path={ROUTES.EQUIPMENT}
             element={<EquipmentDashboard />} />
+          <Route
+            path={ROUTES.LIST_QR_CODES}
+            element={<QRCodes />} />
+          <Route
+            path='/trucks/upload/:id'
+            element={<Upload />} />
         </Routes>
       </div>
     </Router>
