16492e362a5bb3e9d1b9e4693fc44221ab9f3b12
Derion Williams <derionwllms@gmail.com>
2024-01-06 23:33:04 -0600

    Refactored how QR Codes are displayed

--- client/src/pages/QRCodes/QRCodes.tsx
+++ client/src/pages/QRCodes/QRCodes.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState, useRef, useEffect } from "react"
+import { useState, useEffect } from "react"
 import QRCode from "react-qr-code"
 import Sidebar from "components/Sidebar"
 
@@ -12,22 +12,16 @@ function QRCodes() {
     // make a print selected QR function
 
     const currCompanyID = import.meta.env.VITE_COMPANY_ID
-    const [QRData, setQRData] = useState<string[]>([])
-    const [fleetsData, setFleetsData] = useState<any>()
-
-    const canvasRef = useRef<any>(null)
+    const [QRData, setQRData] = useState<{ url: string, name: string }[]>([])
 
     // fetch data
     useEffect(() => {
         const fetchData = async () => {
             try {
-                await axios.get(`${import.meta.env.VITE_SERVER_URL}account/fleet/${currCompanyID}`)
-                    .then(res => {
-                        setFleetsData(res.data)
-                    })
                 await axios.get(`${import.meta.env.VITE_SERVER_URL}print-qr-codes/${currCompanyID}`)
                     .then(res => {
                         setQRData(res.data)
+                        console.log(res.data)
                     })
                     .catch(e => console.error(e))
             } catch (error) {
@@ -38,46 +32,14 @@ function QRCodes() {
         fetchData()
     }, [])
 
-    // create elements for qr codes
-    useEffect(() => {
-        const vehicleNames: string[] = []
-        if (fleetsData) {
-            for (const vehicle of fleetsData.vehicles) {
-                vehicleNames.push(vehicle.name)
-            }
-        }
+    const listQRCodes = QRData.map((vehicle, index) => (
+        <div key={index} className=" inline-block mx-8">
+            <h3>{vehicle.name}</h3>
+            <QRCode value={vehicle.url} />
+            <button>Print</button>
+        </div>
+    ))
 
-        if (QRData.length > 0 && canvasRef.current && canvasRef.current != null) {
-            QRData.forEach((data, index) => {
-                const containerDiv = document.createElement("div")
-                containerDiv.className = " inline-block mx-4"
-                const canvas = document.createElement("canvas")
-
-                QRCode.toCanvas(canvas, data, function (error) {
-                    if (error) console.error(error)
-                    // console.log(`QR Code ${index + 1} generated successfully!`)
-                })
-
-                const QRLabel = document.createElement("p")
-                QRLabel.textContent = vehicleNames[index]
-                QRLabel.className = ""
-
-                const printButton = document.createElement("button")
-                printButton.textContent = "Print"
-                printButton.className = ""
-
-                containerDiv.appendChild(QRLabel)
-                containerDiv.appendChild(canvas)
-                containerDiv.appendChild(printButton)
-                if (canvasRef.current !== null) {
-                    canvasRef.current.appendChild(containerDiv)
-                } else {
-                    console.error('Failed to append QR Codes to containerDiv')
-                }
-            })
-        }
-
-    }, [QRData])
 
     return (
         // display the truck name, QR code, and a print button for every truck created
@@ -87,7 +49,7 @@ function QRCodes() {
                 <div >
                     <h2>QR Codes</h2>
                 </div>
-                <div ref={canvasRef} id="qr-codes-container" className="" />
+                <div id="qr-codes-container" className="">{listQRCodes}</div>
             </div>
         </div>
     )
