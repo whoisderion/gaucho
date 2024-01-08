690140fd418d4bc57764615886a784b09921a30d
Derion Williams <derionwllms@gmail.com>
2024-01-08 09:34:01 -0600

    Fixed env not loading properly

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    proxy: {
-      "/api":  {
-          target:  process.env.VITE_SERVER_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\//, ''),
-        }
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      host: true,
+      proxy: {
+        "/api":  {
+            target:  env.VITE_SERVER_URL,
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          }
+      }
     }
   }
 })
