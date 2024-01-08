1d50faa9114e3c12e428680d4598fcf28173bf4f
Derion Williams <derionwllms@gmail.com>
2024-01-08 01:48:28 -0600

    Updated request routing

--- client/src/pages/Dashboards/EquipmentDashboard/EquipmentDashboard.tsx
+++ client/src/pages/Dashboards/EquipmentDashboard/EquipmentDashboard.tsx
@@ -26,7 +26,7 @@ function EquipmentDashboard() {
 
     useEffect(() => {
         const fetchData = async () => {
-            await axios.get("http://127.0.0.1:4474/account/equipment-complete/" + import.meta.env.VITE_COMPANY_ID).then((res) => {
+            await axios.get(import.meta.env.VITE_SERVER_URL + "api/account/equipment-complete/" + import.meta.env.VITE_COMPANY_ID).then((res) => {
                 setEquipmentData(res.data)
                 console.log(res.data)
                 const defaultRecord = res.data.find((record: { label: string; }) => record.label === "threeDaysAgo");
