917c4780ee19421daf2a5121cd70da4dc1364cf6
Derion Williams <derionwllms@gmail.com>
2024-01-09 19:04:56 -0600

    Created module file for directory

---
+++ client/src/pages/SignUp/index.ts
@@ -0,0 +1,6 @@
+import CreateFleet from "./CreateFleet";
+import CreateInventory from "./CreateInventory";
+import SignUpQRCodes from "./SignUpQRCodes";
+import SignUp from "./SignUp";
+
+export {CreateFleet, CreateInventory, SignUpQRCodes, SignUp}
