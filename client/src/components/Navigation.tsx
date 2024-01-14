4d0e12c81efb419b17255fbda145b5016572b273
Derion Williams <derionwllms@gmail.com>
2024-01-13 23:44:47 -0600

    Conditionally rendered the Sign Out button

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -3,21 +3,26 @@ import { useNavigate } from "react-router-dom"
 import * as ROUTES from "data/routes"
 
 function Navigation() {
-    const {session, signOut} = UseAuth()
-    const navigate = useNavigate()
+	const { session, signOut } = UseAuth()
+	const navigate = useNavigate()
 
-  console.log('session auth:', session)
-  return (
-    <nav className=' border-solid border-ternary border-b-2 '>
-    <h2>Gaucho</h2>
-    <button onClick={() => {
-      signOut
-      navigate(ROUTES.LANDING)
-    }}>
-      Sign Out
-    </button>
-  </nav>
-  )
+	console.log("session auth:", session)
+
+	return (
+		<nav className=' border-solid border-ternary border-b-2 '>
+			<h2>Gaucho</h2>
+			{session !== null && (
+				<button
+					onClick={() => {
+						signOut
+						navigate(ROUTES.LANDING)
+					}}
+				>
+					Sign In
+				</button>
+			)}
+		</nav>
+	)
 }
 
 export default Navigation
